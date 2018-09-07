f0c2f6f4d9273ef5db2a5b253b3f651fcd84752f
Arthur Barbosa <arthurbruno03@gmail.com>
2018-09-07 12:57:33 -0300

    criacao das classe e enums

--- src/main/java/com/arthurbarbosa/helpdesk/api/entity/User.java
+++ src/main/java/com/arthurbarbosa/helpdesk/api/entity/User.java
@@ -1,5 +1,60 @@
 package com.arthurbarbosa.helpdesk.api.entity;
 
-public class User {
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.arthurbarbosa.helpdesk.api.enums.ProfileEnum;
+
+@Document
+public class User {
+	@Id
+	private String id;
+
+	@Indexed(unique = true)
+	@NotBlank(message = "Email required")
+	private String email;
+
+	@NotBlank(message = "Password required")
+	@Size(min = 6)
+	private String password;
+
+	private ProfileEnum profile;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public ProfileEnum getProfile() {
+		return profile;
+	}
+
+	public void setProfile(ProfileEnum profile) {
+		this.profile = profile;
+	}
+	
+	
 }
