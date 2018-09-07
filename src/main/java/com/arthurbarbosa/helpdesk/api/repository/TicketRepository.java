5a79a94ccef2d1e2e02442424a01c3e36f386718
Arthur Barbosa <arthurbruno03@gmail.com>
2018-09-07 13:49:35 -0300

    correcao das interfaces repository e cricao da classe UserService

--- src/main/java/com/arthurbarbosa/helpdesk/api/repository/TicketRepository.java
+++ src/main/java/com/arthurbarbosa/helpdesk/api/repository/TicketRepository.java
@@ -11,13 +11,13 @@ public interface TicketRepository extends MongoRepository<Ticket, String> {
 
 	Page<Ticket> findByUserIdOrderByDateDesc(Pageable pages, String userId);
 
-	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusIgnoreCaseContainingAndPriorityIgnoreCaseContainingOrderByDateDesc(
+	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityOrderByDateDesc(
 			String title, String status, String priority, Pageable pages);
 
-	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusIgnoreCaseContainingAndPriorityIgnoreCaseContainingAndUserIdOrderByDateDesc(
+	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityAndUserIdOrderByDateDesc(
 			String title, String status, String priority, Pageable pages);
 
-	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusIgnoreCaseContainingAndPriorityIgnoreCaseContainingAndAssignedUserOrderByDateDesc(
+	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityAndAssignedUserOrderByDateDesc(
 			String title, String status, String priority, Pageable pages);
 
 	Page<Ticket> findByNumber(Integer number, Pageable pages);
